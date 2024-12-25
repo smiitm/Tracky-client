2bfa4594f7be51fbcfd369a35f819faca3981dd7
smiitm <smitdhameliya2346@gmail.com>
2024-12-25 10:56:11 +0530

    shadcn/ui steup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import './App.css'
-
+import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/mode-toggle"
 function App() {
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div>
         Tracky client it is!
       </div>
-    </>
+      <div>
+        <Button>Click me</Button>
+      </div>
+      <ModeToggle/>
+    </ThemeProvider>
   )
 }
 
 export default App
+
